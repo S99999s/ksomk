04f5b9ae505957dd1f40f8ac9639e2d4ed5f6d83
S99999s <87244655+S99999s@users.noreply.github.com>
2025-05-24 02:40:59 +0300

    Update Demon.py

--- Demon.py
+++ Demon.py
@@ -12,63 +12,63 @@ def Tele(ccx):
     yy = yy.split("20")[1]
   r = requests.session()
   user_agent = ua.random
-  url = "https://api.stripe.com/v1/tokens"
+  import requests
+
+  url = "https://api.stripe.com/v1/payment_methods"
 
   payload = {
-    'guid': "b2baf80b-5b46-433b-9b3e-f4913a8b9549eb9017",
-    'muid': "36647e05-981d-4fd0-8ec6-e10a7d8c6cbe8f3c33",
-    'sid': "39a0de6c-0273-4e7a-b6e2-902d1b91aeaea7e740",
-    'referrer': "https://roamilo.com",
-    'time_on_page': "21996",
-    'card[name]': "aliggjohh",
-    'card[number]': n,
-    'card[cvc]': cvc,
-    'card[exp_month]': mm,
-    'card[exp_year]': yy,
-    'payment_user_agent': "stripe.js/5cceeecc6c; stripe-js-v3/5cceeecc6c; card-element",
-    'pasted_fields': "number",
-    'key': "pk_live_51PE7nGLQnxDNbhLb9v2FASnQFwoYJwbs7LKBdCJls8T8tz7JTVoAD6Mqt23Tpxbjh7Oxibwc9Za79rkqwlkgUG1W00KAzaNVfq"
-  }
+  'card[cvc]': "513",
+  'card[exp_month]': "01",
+  'card[exp_year]': "27",
+  'card[number]': "5218702285726848",
+  'payment_user_agent': "stripe-ios/23.15.0; variant.legacy; STPPaymentCardTextField",
+  'type': "card"
+}
 
   headers = {
-    'User-Agent': "Mozilla/5.0 (iPhone; CPU iPhone OS 18_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.4 Mobile/15E148 Safari/604.1",
-    'Accept': "application/json",
-    'sec-fetch-site': "same-site",
-    'origin': "https://js.stripe.com",
-    'sec-fetch-mode': "cors",
-    'referer': "https://js.stripe.com/",
-    'sec-fetch-dest': "empty",
-    'accept-language': "en-US,en;q=0.9",
-    'priority': "u=3, i"
-  }
+  'User-Agent': "Gios%20Bar%20&%20Grill/264 CFNetwork/3826.500.111.2.2 Darwin/24.4.0",
+  'baggage': "sentry-environment=production,sentry-public_key=224509e7875b449db9212a0388f713a5,sentry-release=dionysus%4036.0.3,sentry-trace_id=78c36399ee204d5cb29728c7327ca349",
+  'authorization': "Bearer pk_live_lYcNhqlKwM4GGRlr4wzTdM04",
+  'accept-language': "en-US,en;q=0.9",
+  'sentry-trace': "78c36399ee204d5cb29728c7327ca349-17dc211206e14628-0",
+  'stripe-version': "2020-08-27",
+  'x-stripe-user-agent': "{\"version\":\"0.31.1\",\"url\":\"https:\\/\\/github.com\\/stripe\\/stripe-react-native\",\"vendor_identifier\":\"FA79543B-5E57-4500-A92A-B093B5258032\",\"type\":\"iPhone12,5\",\"name\":\"@stripe\\/stripe-react-native\\/expo\",\"lang\":\"objective-c\",\"model\":\"iPhone\",\"os_version\":\"18.4\",\"bindings_version\":\"23.15.0\",\"partner_id\":\"pp_partner_JBN7LkABco2yUu\"}"
+}
 
   response = requests.post(url, data=payload, headers=headers)
 
-  id2=(response.json()['id'])
+  id1=(response.json().get('id'))
+import requests
+import json
 
+  url = "https://api.owner.com/customers/KFf9xa71UZgK/locations/3ZUq1h4p4t2R/payment-methods"
 
-  import requests
-  import json
-
-  url = "https://roamilo.api.lifeline.mobi/subscriber/payment_method"
-
-  payload = {
-    "cardToken": id2
+  payload = [
+  {
+    "id": id1,
+    "default": True
+  },
+  {
+    "id": "pm_1RS4tbIZ1PF9tmyNHUa46uMO",
+    "default": False
   }
+]
 
   headers = {
-    'User-Agent': "Mozilla/5.0 (iPhone; CPU iPhone OS 18_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.4 Mobile/15E148 Safari/604.1",
-    'Accept': "application/json, text/plain, */*",
-    'Content-Type': "application/json",
-    'sec-fetch-site': "cross-site",
-    'origin': "https://roamilo.com",
-    'sec-fetch-mode': "cors",
-    'referer': "https://roamilo.com/",
-    'sec-fetch-dest': "empty",
-    'authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwZTMxNjg2ZS1lYjBlLTQwNjUtYmU2MC04M2Q3NjgyZWQwYTIiLCJ1YSI6Ik1vemlsbGEvNS4wIChpUGhvbmU7IENQVSBpUGhvbmUgT1MgMThfNCBsaWtlIE1hYyBPUyBYKSBBcHBsZVdlYktpdC82MDUuMS4xNSAoS0hUTUwsIGxpa2UgR2Vja28pIFZlcnNpb24vMTguNCBNb2JpbGUvMTVFMTQ4IFNhZmFyaS82MDQuMSIsImlwIjoiMTg1LjE4Ny4xMzEuMTU2IiwicHdoIjpudWxsLCJ0eXAiOiJzdWJzY3JpYmVyIiwiaWF0IjoxNzQ4MDAxNTYzLCJleHAiOjE3NDgwMDUxNjN9.MNKaX-k8lWkFSYYhM9rKlcay703eOOFmDeSCd_juaYE",
-    'accept-language': "en-US,en;q=0.9",
-    'priority': "u=3, i"
-  }
+  'User-Agent': "Gios%20Bar%20&%20Grill/264 CFNetwork/3826.500.111.2.2 Darwin/24.4.0",
+  'Accept': "application/json",
+  'Content-Type': "application/json",
+  'x-pb-app-version': "36.0.3",
+  'baggage': "sentry-environment=production,sentry-public_key=224509e7875b449db9212a0388f713a5,sentry-release=dionysus%4036.0.3,sentry-trace_id=78c36399ee204d5cb29728c7327ca349",
+  'x-pb-app-install-uuid': "ea56f4a9-12de-41d0-af35-43d000c28b04",
+  'x-pb-app-build': "264",
+  'authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkaW9ueXN1cyIsImV4cCI6MTc2MzU5NDQzNSwiaWF0IjoxNzQ4MDQyNDM1LCJzdWIiOiJLRmY5eGE3MVVaZ0siLCJ0eXBlIjoiY3VzdG9tZXIiLCJ2IjoxLCJndWVzdElkIjoiT1VBZXRhbm9MN1pZIiwiZ3Vlc3RDU1JGIjoiYTQzREZsVnhGY1NqTE1RRCJ9.qWZDlguIGvACPzUCDi_bNurPLjPRGHBzA2AW9Dx2CW8",
+  'sentry-trace': "78c36399ee204d5cb29728c7327ca349-17dc211206e14628-0",
+  'accept-language': "en-US,en;q=0.9",
+  'x-pb-audience': "dionysus",
+  'x-pb-client': "mobile",
+  'Cookie': "__cf_bm=1FomcdbjoXOFBeawCL135sPhSa2bd.rCFNi2VSK2gZ4-1748042435-1.0.1.1-KrCC8Tn2ZXrNaUE6GBooAAgDapGbcCBvLAzP70nMM7UZHG8xb0ksyOgEZ2BwnoSe1DVGw1H6lZdUtYi977sXsQy3nAlHMwq2rWZTllmCPnKts8TJltnrqLF8reSSuwe3"
+}
 
-  r2 = requests.post(url, data=json.dumps(payload), headers=headers)
+  response = requests.put(url, data=json.dumps(payload), headers=headers)
   return (r2.json())
